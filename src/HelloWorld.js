271723f7d0d5a46bc1e3099af8b907c8de2e2859
Muhammad Ali <muhammad.ali@macys.com>
2022-03-15 17:49:38 +0500

    frontend added

--- src/HelloWorld.js
+++ src/HelloWorld.js
@@ -4,17 +4,13 @@ import axios from "axios";
 const HelloWorld = () => {
   
   const fetchData = () => {
-    return axios.get("http://localhost:8800/")
-       .then((response) => console.log(response.data));
+    return axios.get("http://34.123.61.215:30268/users/")
+       .then((response) => console.log(response.data)
+       );
     }
-
-  // function sayHello() {
-  //   alert('Hello, World!');
-  // }
-  
   return (
     <button onClick={fetchData}>Send GET Request to backend API!</button>
   );
 };
 
-export default HelloWorld;  
+export default HelloWorld;
